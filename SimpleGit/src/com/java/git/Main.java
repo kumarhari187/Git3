da68a862a02b6fcbc8fbcafa4e96e50f401c707c
GitUser <git@user.com>
2016-03-15 14:44:16 +0530

    Initial Checkin.

---
+++ SimpleGit/src/com/java/git/Main.java
@@ -0,0 +1,24 @@
+package com.java.git;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+       Value data = new Value();
+		
+		displayGreeting();
+		data.display();
+	
+	}
+	
+	private static void displayGreeting(){
+		
+		System.out.println("This is Main.java");
+		
+		
+	}
+		
+		
+}
+
+
---
+++ SimpleGit/src/com/java/git/Value.java
@@ -0,0 +1,14 @@
+package com.java.git;
+
+public class Value {
+
+	int data [] = {1,2,3};
+
+	public void display() {
+ 
+		System.out.println("This is Display.java");
+ 
+	}
+	
+	
+}
